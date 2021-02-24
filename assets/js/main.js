58d4860352d53be632b0e74753af23d47a0f329d
Nick Frost <nickfrost.us@gmail.com>
2021-02-23 20:27:58 -0500

    js optimizations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-const classes = ['slide-left', 'slide-right', 'slide-up']
+const html = document.querySelector('html');
+const classes = ['slide-left', 'slide-right', 'slide-up'];
+const slides = document.querySelectorAll('.slide');
 
 function initProjectLinkTransitions() {
     document.querySelectorAll('a').forEach(link => {
@@ -6,7 +8,7 @@ function initProjectLinkTransitions() {
         if (link.href.includes(window.location.origin) && !link.href.includes('/images/') && !link.href.includes('/documents/')) {
             link.addEventListener('click', event => {
                 event.preventDefault();
-                classes.forEach(className => {
+                classes.map(className => {
                     document.querySelectorAll('.' + className + '-inactive').forEach(transition => {
                         transition.classList.add('slide', className);
                     })
@@ -19,21 +21,19 @@ function initProjectLinkTransitions() {
 
 function fadeInPage() {
     return new Promise(resolve => {
-        const html = document.querySelector('html');
         html.classList.add('overflow-x-hidden');
 
-        classes.forEach(className => {
+        classes.map(className => {
             document.querySelectorAll('.' + className).forEach(transition => {
                 transition.classList.add(className + '-inactive');
             });
         })
-        document.querySelectorAll('.slide').forEach(transition => {
+        slides.forEach(transition => {
             transition.classList.remove('slide-left');
             transition.classList.remove('slide-right');
             transition.classList.remove('slide-up');
             setTimeout(() => {
                 html.classList.remove('overflow-x-hidden');
-                transition.classList.remove('slide');
             }, 150);
         })
         resolve();
